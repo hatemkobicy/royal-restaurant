9de4d17e7f3f869c91bed23a9f6ae7861dffa331
Hatem Kobicy <62618939+hatemkobicy@users.noreply.github.com>
2025-05-29 17:59:58 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+// import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import RootLayout from "@/components/RootLayout";
@@ -83,7 +83,6 @@ function App() {
       <SettingsProvider>
         <LanguageProvider>
           <TooltipProvider>
-            <Toaster />
             <Router />
           </TooltipProvider>
         </LanguageProvider>
